c8b667d0c3f69d04d12ec535410212992e8e3aa7
unknown <david.jaroska@gmail.com>
2020-02-15 19:57:04 +0100

    few changes to html and script

--- js/movies.js
+++ js/movies.js
@@ -6,7 +6,7 @@ for (let i=0; i < movies.length; i++){
 	var likes =Math.floor(Math.random() * 26);
 
 	var img = ` <div id="div${i}" class="col-lg-5 col-md-6 m-5  movieframe">
-						<img class="poster img-fluid img-responsive" id="image${i}" src="${movies[i].img}">
+						<img class="poster img-responsive" id="image${i}" src="${movies[i].img}">
 					<div class="text mh-100">	
 						<h2 class"title text-center"> ${movies[i].name}</h2>		
 						<p class="text-white text-left pl-2 w-100 h-20"  id="p${i}">Directed by: ${movies[i].director}</p>
