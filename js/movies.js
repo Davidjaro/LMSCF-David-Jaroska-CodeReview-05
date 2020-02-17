1c2bf6bdfadd26ec4066544060b343f535179b76
unknown <david.jaroska@gmail.com>
2020-02-17 18:49:57 +0100

    changed img size was not compatible with other browsers

--- js/movies.js
+++ js/movies.js
@@ -34,19 +34,20 @@ function leastArray(){
 for (let i=0; i < movies.length; i++){
 	likes[i] = Number(movies[i].likes);
 
-	$(".movies").append(` <div class="col-12 col-sm-6 col-lg-4 mb-4 mt-2 ">
+	$(".movies").append(` 
+		<div class="col-12 col-sm-6 col-lg-4 mb-4 mt-2 ">
             <div class="movieframe text-white">
                 <div class="container-fluid">
                     <div class="row bg-secondary">
                         <div class="col-5 p-2">
-                            <img class="rounded border" src="${movies[i].img}" alt="">
+                            <img class="rounded border movieposter" src="${movies[i].img}" alt="">
                         </div>
                         <div class="col-7 d-flex flex-column">
                             <h3 class="mt-2 ">${movies[i].name}</h3>
                             <p class="mt-1 ">${movies[i].desc}</p>
                             <button class="bg-transparent thumbbtn border-0 ml-auto mt-auto p-0 mb-3 d-flex pr-4">
                                 <span class="pr-4">Like</span>
-                                <img class="pr-4 d-inline" src="img/likebtn.svg" alt="">
+                                <img class="pr-4 d-inline likepic" src="img/likebtn.svg" alt="">
                                 <span class="likes rounded-circle text-white pr-4">${movies[i].likes}</span>
                             </button>
                         </div>
